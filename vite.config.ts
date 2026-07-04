5e40e4f325e0a2990845cb52275fac4e04a88061
dnhunteram-code <dnhunteram@gmail.com>
2026-07-04 23:05:57 +0300

    Remove Electron config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
   plugins: [
-    electron({
-      main: {
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      renderer: process.env.NODE_ENV === 'test'
-        ? undefined
-        : {},
-    }),
-  ],
-})
+    glsl()
+  ]
+});
